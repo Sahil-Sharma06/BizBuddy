6199fe91bfe0b4547c40353a364eaf12d6c5f4b8
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-06-22 03:12:46 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar"
+import { TypewriterEffectSmoothDemo } from "./Components/TypewriterEffectSmoothDemo"
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <TypewriterEffectSmoothDemo/>
+    </>
+  )
+}
+
+export default App
